197411ff56149887a1efea63bdca56bd51753d20
Emil Fattakhov <emil.e.fattakhov@gmail.com>
2020-08-04 11:58:11 -0700

    Add CRUD / User autentication / Reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see 
+  # https://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  get "/", to: "ideas#index", as: 'root'
+
+  resources :ideas do 
+    resources :reviews, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+  
+  
+
 end
